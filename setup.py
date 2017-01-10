de3ce3647c62d255d0adf9ff66ce4b8866994adb
wilspi <thewilspi@gmail.com>
2017-01-10 15:31:40 +0530

    REMOVE: manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
 setup(name='grapheap',
       version='0.1',
       description='Read optimised graph, inspired by heap',
-      long_description=readme(),
+      long_description='Read http://github.com/practo/grapheap',
       keywords='grapheap graph heap python node memcache',
       url='http://github.com/practo/grapheap',
       author='Sourabh Deokar (wilspi)',
